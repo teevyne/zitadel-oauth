a8a1f909b8edceda36d15bdf0a8f2cb74b63655f
Tolulope Ayemobola <ayemobolatolulope@gmail.com>
2022-07-22 17:43:15 +0100

    initial commit

---
+++ src/test/java/com/oauth/zitadel/ZitadelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth.zitadel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZitadelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
